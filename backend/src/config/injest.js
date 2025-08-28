7ab08149162637712932f2a9fb38b6034aad468d
Waqar-Hafeez-Ahmed <you@example.com>
2025-08-28 16:14:03 +0500

    Fixes quick bugs

--- backend/src/config/injest.js
+++ backend/src/config/injest.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import { connectDB } from "./db";
+import { connectDB } from "./db.js";
 import { User } from "../models/User.js";
 
 // Create a client to send and receive events
