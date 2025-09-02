457d468b4cc5a8c3f6adae8213da57fbc02682f2
Waqar-Hafeez-Ahmed <you@example.com>
2025-09-02 13:39:03 +0500

    Revert "fixxing errors"
    This reverts commit f4add6365149bb7549a5d5f2f5a331e33007b902.

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,27 +1,14 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
+const UserSchema = new mongoose.Schema(
   {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    image: {
-      type: String,
-      required: true,
-    },
-    clerkId: {
-      type: String,
-      required: true,
-      unique: true,
-    },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    image: { type: String, required: true },
+    clerkId: { type: String, required: true, unique: true },
   },
   { timestamps: true }
 );
 
-export const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", UserSchema);
+export default User;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,47 +1,43 @@
 // const express = require("express"); //type:common.js
-import "../instrument.mjs";
+// import "../instrument.mjs";
 import express from "express"; //type:module
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { functions, inngest } from "./config/injest.js";
-import chatRoutes from "./routes/chat.route.js";
 import { serve } from "inngest/express";
-import cors from "cors";
+import chatRoutes from "./routes/chat.route.js";
 import * as Sentry from "@sentry/node";
 
 const app = express();
-app.use(express.json());
-// const PORT = ENV.PORT;
+const PORT = ENV.PORT;
 
+app.use(express.json());
 app.use(clerkMiddleware()); //req.auth will be available
 app.get("/debug-sentry", (req, res) => {
   throw new Error("Debug first Sentry");
 });
-
 app.get("/", (req, res) => {
-  res.send("Hello World! 123");
+  res.send(`Slack Clone Backend PORT ${PORT}`);
 });
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
-app.use("/api/chat", chatRoutes);
-
+app.use("/api/inngest", serve({ client: inngest, functions })); //handles ingest
+app.use("/api/chat", chatRoutes); //handles stream
 Sentry.setupExpressErrorHandler(app);
 
 const startServer = async () => {
   try {
     await connectDB();
     if (ENV.NODE_ENV !== "production") {
-      app.listen(ENV.PORT, () => {
-        console.log("Server started on port:", ENV.PORT);
+      app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
       });
-    }
+    } //if we're in development run on port
   } catch (error) {
     console.error("Error starting server:", error);
-    process.exit(1); // Exit the process with a failure code
+    process.exit(1);
   }
 };
-
 startServer();
 
 export default app;
