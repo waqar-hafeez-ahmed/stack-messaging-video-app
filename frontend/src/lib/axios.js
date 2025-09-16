61f647d61a44066d6199a6adc30e5d1925264a98
Waqar-Hafeez-Ahmed <you@example.com>
2025-09-16 13:34:50 +0500

    auth provider setup and sentry FE setup

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,15 @@
+// https://stack-messaging-video-app.vercel.app/
+import axios from "axios";
+
+// this was the first version, we just changed it at the end of the tutorial
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://stack-messaging-video-app.vercel.app/api";
+
+// const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,54 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router";
 import { ClerkProvider } from "@clerk/clerk-react";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+import AuthProvider from "./provider/AuthProvider.jsx";
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const queryClient = new QueryClient();
+
+import * as Sentry from "@sentry/react";
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.reactRouterV7BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+          <Toaster position="top-right" />
+        </QueryClientProvider>
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
