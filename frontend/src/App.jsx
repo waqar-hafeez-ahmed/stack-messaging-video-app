733a7bdb7ea50c2a19c1ba09048dec91b69695d9
Waqar-Hafeez-Ahmed <you@example.com>
2025-09-16 12:36:23 +0500

    auth page created and implimented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,15 +6,24 @@ import {
   UserButton,
   SignInButton,
 } from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
 const App = () => {
   return (
     <header>
-      <SignedOut>
-        <SignInButton mode="modal" />
-      </SignedOut>
       <SignedIn>
-        <UserButton />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<Navigate to="/" replace />} />
+        </Routes>
       </SignedIn>
+      <SignedOut>
+        <Routes>
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="*" element={<Navigate to="/auth" replace />} />
+        </Routes>
+      </SignedOut>
     </header>
   );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
 import { ClerkProvider } from "@clerk/clerk-react";
+import "./index.css";
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -12,7 +14,9 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
 );
